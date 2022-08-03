e11af2e05e6adca33fabbc7e369f8483bb50466d
Damon Leven <dleven@protonmail.com>
2022-08-03 03:11:55 +0200

    cleaned up main

--- console/main.cpp
+++ console/main.cpp
@@ -1,23 +1,9 @@
-#include <iostream>
-#include <console/chess_tui.hpp>
 #include <cpp-terminal/base.hpp>
-#include "visual.hpp"
 #include "menu.hpp"
 
 int main() {
     // prepare the terminal
-    Term::Terminal term(true, true, false, true);
-
-    int rows, cols;
-    Term::get_term_size(rows, cols);
-
-    // make the term size uneven
-    if(rows % 2 == 0) {
-        rows--;
-    }
-    if(cols % 2 == 0) {
-        cols--;
-    }
+    Term::Terminal term(true, true, true, true);
 
     Menu::Main_menu mainMenu;
     mainMenu.start();
